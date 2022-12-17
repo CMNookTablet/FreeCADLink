f3db9d2fc25a3c202f29523df78f5629650f459f
Kuzemko Alexsandr <1382812+Kuzma30@users.noreply.github.com>
2022-12-17 21:07:48 +0200

    New translations TestRT.ts (Russian)

--- src/Mod/Test/Gui/Resources/translations/Test_ru.ts
+++ src/Mod/Test/Gui/Resources/translations/Test_ru.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="17"/>
       <source>FreeCAD UnitTest</source>
-      <translation>FreeCAD UnitTest</translation>
+      <translation type="unfinished">FreeCAD UnitTest</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="32"/>
@@ -26,7 +26,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="86"/>
       <source>Alt+S</source>
-      <translation>Alt+S</translation>
+      <translation type="unfinished">Alt+S</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="115"/>
@@ -36,7 +36,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="118"/>
       <source>F1</source>
-      <translation>F1</translation>
+      <translation type="unfinished">F1</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="128"/>
@@ -46,7 +46,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="131"/>
       <source>Alt+A</source>
-      <translation>Alt+A</translation>
+      <translation type="unfinished">Alt+A</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="141"/>
@@ -56,7 +56,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="144"/>
       <source>Alt+C</source>
-      <translation>Alt+C</translation>
+      <translation>ALT+C</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="162"/>
@@ -109,13 +109,13 @@
     <message>
       <location filename="../../UnitTestImp.cpp" line="152"/>
       <source>Enter the name of a callable object which, when called, will return a TestCase.
-Click 'start', and the test thus produced will be run.
+Click &apos;start&apos;, and the test thus produced will be run.
 
 Double click on an error in the tree view to see more information about it, including the stack trace.</source>
-      <translation>Введите имя вызываемого объекта при вызове которого, будет возвращаться TestCase.
-Кликните 'Запустить' и, тест будет запущен.
+      <translation type="unfinished">Enter the name of a callable object which, when called, will return a TestCase.
+Click &apos;start&apos;, and the test thus produced will be run.
 
-Сделайте двойной щелчок на ошибке в древовидном списке для дополнительной информации о ней, включая стек вызовов.</translation>
+Double click on an error in the tree view to see more information about it, including the stack trace.</translation>
     </message>
     <message>
       <location filename="../../UnitTestImp.cpp" line="164"/>
