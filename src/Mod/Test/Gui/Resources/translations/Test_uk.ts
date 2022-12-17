a9671a8d913eec3f120b8494e3c95c685896111b
Kuzemko Alexsandr <1382812+Kuzma30@users.noreply.github.com>
2022-12-17 21:34:06 +0200

    New translations TestRT.ts (Ukrainian)

--- src/Mod/Test/Gui/Resources/translations/Test_uk.ts
+++ src/Mod/Test/Gui/Resources/translations/Test_uk.ts
@@ -26,7 +26,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="86"/>
       <source>Alt+S</source>
-      <translation>Alt+S</translation>
+      <translation type="unfinished">Alt+S</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="115"/>
@@ -36,7 +36,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="118"/>
       <source>F1</source>
-      <translation>F1</translation>
+      <translation type="unfinished">F1</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="128"/>
@@ -46,7 +46,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="131"/>
       <source>Alt+A</source>
-      <translation>Alt+A</translation>
+      <translation type="unfinished">Alt+A</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="141"/>
@@ -56,7 +56,7 @@
     <message>
       <location filename="../../UnitTest.ui" line="144"/>
       <source>Alt+C</source>
-      <translation>Alt+C</translation>
+      <translation type="unfinished">Alt+C</translation>
     </message>
     <message>
       <location filename="../../UnitTest.ui" line="162"/>
@@ -109,13 +109,13 @@
     <message>
       <location filename="../../UnitTestImp.cpp" line="152"/>
       <source>Enter the name of a callable object which, when called, will return a TestCase.
-Click 'start', and the test thus produced will be run.
+Click &apos;start&apos;, and the test thus produced will be run.
 
 Double click on an error in the tree view to see more information about it, including the stack trace.</source>
-      <translation>Введіть ім'я викликається об'єкта при виклику якого, буде повертатися TestCase.
-Клацніть 'Запуск' і, тест буде запущений.
+      <translation type="unfinished">Enter the name of a callable object which, when called, will return a TestCase.
+Click &apos;start&apos;, and the test thus produced will be run.
 
-Зробіть подвійне клацання на помилку в деревовидному списку для додаткової інформації про неї, включаючи стек викликів.</translation>
+Double click on an error in the tree view to see more information about it, including the stack trace.</translation>
     </message>
     <message>
       <location filename="../../UnitTestImp.cpp" line="164"/>
